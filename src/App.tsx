4138ceeb6bef3bbf9c4da352ffd80efeac8feecf
Catargo <cobauper@gmail.com>
2024-10-17 17:16:00 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./main.css";
+
+function App() {
+  return (
+    <div className="flex justify-center mt-12">
+      <div className=" rounded-xl lg:w-[600px] w-screen aspect-video bg-[#363636]"></div>
+    </div>
+  );
+}
+
+export default App;
